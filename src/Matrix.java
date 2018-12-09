dd2eb904217f88c4c6ef5f4494a054d0f0fdc761
София <sofiro@ukr.net>
2018-12-09 17:12:50 +0200

    Matritsa s odinakovumi v raznux strokax chislami

---
+++ src/Matrix.java
@@ -0,0 +1,13 @@
+public class Matrix {
+    public static void main(String[] args) {
+        int columns = 2;
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            System.out.println();
+            for (int j = 1; j <= columns; j++) {
+                System.out.print(j + " ");
+            }
+        }
+
+    }
+}
