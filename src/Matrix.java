198a23209bc2b2b1e939dff31c716de28f4562e6
София <sofiro@ukr.net>
2018-12-10 21:16:53 +0200

    Pravilnaya Matritsa ne otformatirovanaya

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,12 +1,14 @@
 public class Matrix {
     public static void main(String[] args) {
-        int columns = 2;
+        int columns = 5;
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
+
+        for (int i = 0; i < rows; i++) {
             System.out.println();
-            for (int j = 1; j <= columns; j++) {
-                System.out.print(j + " ");
+            for (int j = 0; j < columns; j++) {
+                System.out.print(i * columns + j + 1 + " ");
             }
+
         }
 
     }
