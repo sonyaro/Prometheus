cd412982b5db81ea64c079fbdedb19b1a93b8da2
София <sofiro@ukr.net>
2018-12-15 14:21:00 +0200

    Pravilnaya Matritsa ne otformatirovanaya so zvezdochcami 2

--- src/Matrix.java
+++ src/Matrix.java
@@ -6,8 +6,13 @@ public class Matrix {
         for (int i = 0; i < rows; i++) {
             System.out.println();
             for (int j = 0; j < columns; j++) {
+/*                if (i * columns + j + 1 < 10){
+                    System.out.print(" " +( i * columns + j + 1) + " ");
+                }else {
+                    System.out.print(i * columns + j + 1 + " ");
+                }*/
                 if (i == j || i == 4 - j){
-                    System.out.print("* ");
+                    System.out.print(" * ");
                 }else {
                     System.out.print(i * columns + j + 1 + " ");
                 }
